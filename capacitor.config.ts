d4fd9a015b97d1147c912ec957e4fc499db93521
Ria Pacheco <me@riapacheco.com>
2023-02-05 20:20:06 -0700

    Configured output directory path file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.riaudio.app',
+  appName: 'ri-audio-api',
+  webDir: 'dist/ri-audio-api',
+  bundledWebRuntime: false
+};
+
+export default config;
